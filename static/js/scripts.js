f27240d5b58f38b29173c7a4c23c179371d28f6f
unknown <jm.jamrot@gmail.com>
2023-07-30 21:37:39 +0800

    20230730

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,14 @@
 document.getElementById('chat-input').addEventListener('keydown', function(e) {
     if (e.key === 'Enter') {
-        sendMessage();
+        if (e.shiftKey){
+        }
+        else {
+            sendMessage();
+            e.preventDefault();
+        }
     }
 });
+
 
 let chatHistory = []; // Array to store chat history
 let currentChat = []; // Array to store current chat
@@ -76,6 +82,7 @@ document.getElementById("new-chat-button").addEventListener("click", function() 
     currentChat = [];
 });
 
+let architecture = "x86_32";  // Set default optimization level
 
 document.querySelectorAll("#system-buttons button").forEach(button => {
     button.addEventListener("click", function() {
@@ -86,44 +93,101 @@ document.querySelectorAll("#system-buttons button").forEach(button => {
         // Add selected class to the clicked button
         button.classList.add("selected");
         
-        selectSystem(button.textContent);
+        // selectSystem(button.textContent);
+        architecture = button.textContent
+    });
+});
+
+let optimization_level = "O1";  // Set default optimization level
+
+document.querySelectorAll("#opt-buttons button").forEach(button => {
+    button.addEventListener("click", function() {
+        // Remove selected class from previously selected button
+        document.querySelectorAll("#opt-buttons button.selected")
+            .forEach(btn => btn.classList.remove("selected"));
+        
+        // Add selected class to the clicked button
+        button.classList.add("selected");
+        
+        // selectSystem(button.textContent);
+        optimization_level = button.textContent;
     });
 });
 
 window.addEventListener('load', () => {
-    addBotMessage("Hello! How can I help you today?");
+    addBotMessage("Hello! Please input your binary code.");
 });
 
 function sendMessage() {
     const inputElement = document.getElementById('chat-input');
     const chatBoxElement = document.getElementById('chat-box');
+    const added = 0;
 
     if (inputElement.value.trim() !== '') {
         const messageElement = document.createElement('div');
         messageElement.classList.add('message', 'user-message');
-        messageElement.textContent = inputElement.value;
+        // messageElement.textContent = inputElement.value;
+        messageElement.innerHTML = inputElement.value.replace(/\n/g, '<br>');
 
         // Add user icon
         const userIcon = document.createElement('i');
         userIcon.classList.add('fas', 'fa-user', 'icon');
         messageElement.insertBefore(userIcon, messageElement.firstChild);
-        userIcon.style.marginRight = '15px'; // 添加右侧间距
+        userIcon.style.marginRight = '15px';
 
         // Add message to current chat array
         currentChat.push({type: 'user', content: inputElement.value});
 
         chatBoxElement.appendChild(messageElement);
 
+        // Prepare data to send to the server
+        const assembly_code_input = inputElement.value;
+        const data = {
+            assembly_code_input: assembly_code_input,
+            optimization_level: optimization_level,
+            architecture: architecture
+        };
+
+        const loadingMsg =addBotMessage('generating&nbsp&nbsp<span class="dots"><span>.&nbsp</span><span>.&nbsp</span><span>.&nbsp</span></span>')
+
+        // Send data to the server and handle the response
+        const fetchData = fetch('http://47.96.18.113:5000/app', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        });
+
+        const timeout = new Promise((resolve, reject) => {
+            setTimeout(reject, 20000, 'Request timed out');
+        });
+
+        Promise.race([fetchData, timeout])
+        .then(response => response.json())
+        .then(data => {
+            const bcs_summary = data.bcs_summary;
+            updateBotMessage(loadingMsg, bcs_summary);
+        })
+        .catch((error) => {
+            if (error === 'Request timed out') {
+                updateBotMessage(loadingMsg, '<span style="color: red;">FAILED (TIMEOUT)</span>');
+            } else {
+                console.error('Error:', error);
+            }
+        });
+
         // Sample responses
-        if (inputElement.value.toLowerCase().includes("hello")) {
-            addBotMessage("Hi there!");
-        } else if (inputElement.value.toLowerCase().includes("how are you")) {
-            addBotMessage("I'm a chatbot, so I don't have feelings, but I'm here to help you!");
-        } else {
-            addBotMessage("I'm not sure how to respond to that. Can you please provide more information?");
-        }
+        // if (inputElement.value.toLowerCase().includes("hello")) {
+        //     addBotMessage("Hi there!");
+        // } else if (inputElement.value.toLowerCase().includes("how are you")) {
+        //     addBotMessage("I'm a chatbot, so I don't have feelings, but I'm here to help you!");
+        // } else if (added==0) {
+        //     addBotMessage("I'm not sure how to respond to that. Can you please provide more information?");
+        // }
 
         inputElement.value = '';
+        chatBoxElement.scrollTop = chatBoxElement.scrollHeight;
     }
 }
 
@@ -131,17 +195,28 @@ function addBotMessage(message) {
     const chatBoxElement = document.getElementById('chat-box');
     const messageElement = document.createElement('div');
     messageElement.classList.add('message', 'bot-message');
-    messageElement.textContent = message;
+    messageElement.innerHTML = message;
 
     const botIcon = document.createElement('i');
     botIcon.classList.add('fas', 'fa-code', 'icon');
     messageElement.insertBefore(botIcon, messageElement.firstChild);
-    botIcon.style.marginRight = '10px'; // 添加右侧间距
+    botIcon.style.marginRight = '10px';
 
     chatBoxElement.appendChild(messageElement);
 
     // Add message to current chat array
     currentChat.push({type: 'bot', content: message});
+    return messageElement;
+}
+
+function updateBotMessage(messageElement, newMessage) {
+    messageElement.innerHTML = newMessage;
+
+    // If your messages include the bot icon, you need to add it back after changing the text content
+    const botIcon = document.createElement('i');
+    botIcon.classList.add('fas', 'fa-code', 'icon');
+    messageElement.insertBefore(botIcon, messageElement.firstChild);
+    botIcon.style.marginRight = '10px';
 }
 
 function sendExample(message) {
@@ -151,6 +226,9 @@ function sendExample(message) {
 }
 
 function clearChat() {
-    document.getElementById("chat-box").innerHTML = '';
+    var result = confirm("CLEAR?");
+    if (result) {
+        document.getElementById("chat-box").innerHTML = '';
+    }
 }
 
